19f5bc510de74d494c85f61646d455714a444114
Kevin Curry <kmcurry@gmail.com>
2019-02-18 22:12:01 -0500

    add more tests

--- tests/tests.py
+++ tests/tests.py
@@ -7,18 +7,22 @@ class MyFirstTests(unittest.TestCase):
 
     def test_should_convert_to_iso8601(self):
         convert_to_iso8601("4/1/11 11:00:00 AM")
+    
+    def test_should_drop_row_for_date_error(self):
+        try:
+            convert_to_iso8601("4/1/11🍏AM")
+        except:
+            pass
 
     def test_should_normalize_zipcode(self):
         normalize_zipcode("0102")
 
     def test_should_normalize_name(self):
         normalize_name("kevin curry")
+    
+    def test_should_normalize_addr(self):
+        normalize_addr("23 Gangnam, Style Lives Here, Gangnam Town, USA")
 
-    def test_should_drop_row_for_date_error(self):
-        try:
-            convert_to_iso8601("4/1/11🍏AM")
-        except:
-            pass
 
 if __name__ == '__main__':
     unittest.main()
