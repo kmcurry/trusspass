5e8c8cf98a228fb6fce1783f1a2dcc5b945640ae
Kevin Curry <kmcurry@gmail.com>
2019-02-18 22:00:09 -0500

    add tests for name and zip

--- tests/tests.py
+++ tests/tests.py
@@ -8,6 +8,12 @@ class MyFirstTests(unittest.TestCase):
     def test_should_convert_to_iso8601(self):
         convert_to_iso8601("4/1/11 11:00:00 AM")
 
+    def test_should_normalize_zipcode(self):
+        normalize_zipcode("0102")
+
+    def test_should_normalize_name(self):
+        normalize_name("kevin curry")
+
     def test_should_drop_row_for_date_error(self):
         try:
             convert_to_iso8601("4/1/11🍏AM")
